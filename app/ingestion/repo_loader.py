c49bed355fb29702349e10659769012a616d4508
MD AKRAM <akramshahjada786@gmail.com>
2026-03-13 00:27:23 +0530

    feat: implement repository code loader with extension filtering

--- app/ingestion/repo_loader.py
+++ app/ingestion/repo_loader.py
@@ -3,6 +3,9 @@ import git
 
 BASE_REPO_PATH = "data/repos"
 
+SUPPORTED_EXTENSIONS = [".py", ".js", ".ts", ".tsx", ".cpp", ".java"]
+
+
 def clone_repository(repo_url: str):
 
     os.makedirs(BASE_REPO_PATH, exist_ok=True)
@@ -16,3 +19,33 @@ def clone_repository(repo_url: str):
     git.Repo.clone_from(repo_url, repo_path)
 
     return repo_path
+
+
+def load_code_files(repo_path):
+
+    code_files = []
+
+    for root, dirs, files in os.walk(repo_path):
+
+        # prevent scanning useless directories
+        dirs[:] = [d for d in dirs if d not in ["node_modules", ".git", "__pycache__"]]
+
+        for file in files:
+
+            if any(file.endswith(ext) for ext in SUPPORTED_EXTENSIONS):
+
+                file_path = os.path.join(root, file)
+
+                try:
+                    with open(file_path, "r", encoding="utf-8") as f:
+                        content = f.read()
+
+                    code_files.append({
+                        "path": file_path,
+                        "content": content
+                    })
+
+                except Exception:
+                    continue
+
+    return code_files
